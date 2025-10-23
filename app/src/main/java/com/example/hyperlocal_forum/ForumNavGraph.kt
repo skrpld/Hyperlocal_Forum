392857f79747a89c1641eb310ba6ac8d25394f94
skrpld <skrpld96@gmail.com>
2025-10-23 22:16:40 +0300

    Update ForumNavGraph.kt

--- app/src/main/java/com/example/hyperlocal_forum/ForumNavGraph.kt
+++ app/src/main/java/com/example/hyperlocal_forum/ForumNavGraph.kt
@@ -1,17 +1,11 @@
 package com.example.hyperlocal_forum
 
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import androidx.navigation.NavType
@@ -21,14 +15,9 @@ import com.example.hyperlocal_forum.ui.profile.ProfileScreen
 import com.example.hyperlocal_forum.ui.topic.detail.TopicDetailScreen
 import com.example.hyperlocal_forum.ui.topic.edit.TopicEditScreen
 import com.example.hyperlocal_forum.ui.topic.TopicsScreen
-import com.example.hyperlocal_forum.ui.components.AppDrawer
-import kotlinx.coroutines.CoroutineScope
-
 @Composable
 fun ForumNavGraph(
     modifier: Modifier = Modifier,
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
-    drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed),
     navController: NavHostController = rememberNavController(),
     isLoggedIn: Boolean,
     authManager: AuthManager,
